bc03f19e7b2a7815b61cace3a433eb85fee55b25
Benoit Koenig <koenigbenoit47@gmail.com>
2025-01-24 18:24:19 +0100

    Add a router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import AnonymousData from "./providers/AnonymousData.vue";
 import ProfileData from "./providers/ProfileData.vue";
-import AnonymousDailyForm from "./views/AnonymousDailyForm.vue";
 </script>
 
 <template>
   <AnonymousData>
     <ProfileData>
-      <AnonymousDailyForm />
+      <router-view />
     </ProfileData>
   </AnonymousData>
 </template>
