979ea8eb1c68f17561651749a47fdd6b35041f9e
Benoit Koenig <koenigbenoit47@gmail.com>
2025-01-24 17:26:59 +0100

    Add the QuestionBloc component

---
+++ src/components/QuestionBlock.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ title: string }>();
+
+const isExpanded = ref(true);
+
+function collapse() {
+  isExpanded.value = false;
+}
+
+function expand() {
+  isExpanded.value = true;
+}
+</script>
+
+<template>
+  <div class="w-full flex flex-col p-4 gap-4 border">
+    <div class="w-full flex gap-4">
+      <h3 class="flex-grow">{{ title }}</h3>
+      <button v-if="$slots.collapsed && isExpanded" @click="collapse">V</button>
+      <button v-else-if="$slots.collapsed" @click="expand">&lt;</button>
+    </div>
+    <div v-show="isExpanded" class="w-full">
+      <slot></slot>
+    </div>
+    <div v-show="!isExpanded" class="w-full">
+      <slot name="collapsed"></slot>
+    </div>
+  </div>
+</template>
--- src/views/AppViews.vue
+++ src/views/AppViews.vue
@@ -1,21 +1,38 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { ref } from "vue";
 
 import AllergenPicker from "../components/AllergenPicker.vue";
 import CityPicker from "../components/CityPicker.vue";
-import { profileDataProviderKey } from "../providers/ProfileData.key";
+import QuestionBlock from "../components/QuestionBlock.vue";
+import { type Allergen } from "../static-data/allergens.static-data";
 
-const { latestLocation, setLatestLocation } = inject(profileDataProviderKey)!;
+const latestLocation = ref();
+const knownAllergies = ref<Allergen[]>([]);
+
+function setLatestLocation(value: string) {
+  latestLocation.value = value;
+}
+
+function updateKnownAllergies(value: Allergen[]) {
+  knownAllergies.value = value;
+}
 </script>
 
 <template>
   <div class="relative w-full h-full">
     <div
-      class="absolute inset-y-0 inset-x-1/3 flex flex-col justify-evenly items-center"
+      class="absolute inset-y-0 inset-x-1/3 flex flex-col gap-4 items-center"
     >
-      <div v-if="latestLocation">Picked city: {{ latestLocation }}</div>
-      <CityPicker :default-value="latestLocation" @select="setLatestLocation" />
-      <AllergenPicker />
+      <QuestionBlock title="What is your current location?">
+        <CityPicker
+          :default-value="latestLocation"
+          @select="setLatestLocation"
+        />
+      </QuestionBlock>
+      <QuestionBlock title="What are your known allergies?">
+        <AllergenPicker @update="updateKnownAllergies" />
+        <template #collapsed>{{ knownAllergies.join(", ") }}</template>
+      </QuestionBlock>
     </div>
   </div>
 </template>
