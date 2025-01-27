81c3d4db9b73e9b939741610de295052f38fddda
Benoit Koenig <koenigbenoit47@gmail.com>
2025-01-27 12:02:22 +0100

    Add a unit test on QuestionBlock

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  test: {
+    environment: "happy-dom",
+  },
 });
