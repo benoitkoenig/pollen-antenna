3f939bd456b5fbd3f243e67c5b6014992083db66
benoit <benoit.koenig@accounting-for.accor.com>
2025-11-15 23:28:35 +0100

    Fix the populate-subdivisions script

--- data-storage/scripts/populate-subdivisions/map-feature-to-subdivision.ts
+++ data-storage/scripts/populate-subdivisions/map-feature-to-subdivision.ts
@@ -3,17 +3,17 @@ import type { Feature } from "./fetch-features";
 export default function mapFeatureToSubdivision(feature: Feature) {
   const coordinates =
     feature.geometry.type === "Polygon"
-      ? [feature.geometry.coordinates.map((a) => a.flat())]
+      ? [feature.geometry.coordinates.flat()]
       : feature.geometry.coordinates.map((a) => a.flat());
 
-  let northBound = coordinates[0][0][0] as number;
-  let southBound = coordinates[0][0][0] as number;
-  let eastBound = coordinates[0][0][1] as number;
-  let westBound = coordinates[0][0][1] as number;
+  let northBound = coordinates[0][0][1] as number;
+  let southBound = coordinates[0][0][1] as number;
+  let eastBound = coordinates[0][0][0] as number;
+  let westBound = coordinates[0][0][0] as number;
 
   for (const a of coordinates) {
     for (const b of a) {
-      const [lat, lng] = b as [number, number];
+      const [lng, lat] = b as [number, number];
 
       if (lat > northBound) {
         northBound = lat;
