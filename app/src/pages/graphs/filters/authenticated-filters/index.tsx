ebda77236d2eb8611975b40f0c26c04241afbdba
benoit <benoit.koenig@accounting-for.accor.com>
2025-11-17 21:19:27 +0100

    Change the design of the filters button

--- app/src/pages/graphs/filters/authenticated-filters/index.tsx
+++ app/src/pages/graphs/filters/authenticated-filters/index.tsx
@@ -1,9 +1,13 @@
 import { memo, useCallback, useState } from "react";
 import { FormattedMessage } from "react-intl";
 
+import { useFilters } from "pages/graphs/graphs-providers/filters-provider";
+
 import { FiltersModal } from "./filters-modal";
 
 export const AuthenticatedFilters = memo(function AuthenticatedFilters() {
+  const { allergen } = useFilters();
+
   const [modalIsVisible, setModalIsVisible] = useState(false);
 
   const openModal = useCallback(() => {
@@ -16,12 +20,30 @@ export const AuthenticatedFilters = memo(function AuthenticatedFilters() {
 
   return (
     <div>
-      <button
-        onClick={openModal}
-        className="px-4 py-2 rounded bg-blue-600 text-white cursor-pointer hover:bg-blue-700"
-      >
-        <FormattedMessage defaultMessage="Edit filters" description="Filters" />
-      </button>
+      <div>
+        {allergen ? (
+          <span className="me-2">
+            <FormattedMessage
+              defaultMessage="Filtered by {allergen}"
+              description="Filters"
+              values={{ allergen }}
+            />
+          </span>
+        ) : null}
+        <button
+          onClick={openModal}
+          className="px-4 py-2 rounded bg-blue-600 text-white cursor-pointer hover:bg-blue-700"
+        >
+          {allergen ? (
+            <FormattedMessage defaultMessage="Change" description="Filters" />
+          ) : (
+            <FormattedMessage
+              defaultMessage="Edit filters"
+              description="Filters"
+            />
+          )}
+        </button>
+      </div>
       {modalIsVisible ? <FiltersModal onClose={closeModal} /> : null}
     </div>
   );
