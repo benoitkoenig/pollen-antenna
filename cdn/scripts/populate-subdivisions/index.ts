3a86c10edcfb7e8d5db44f2df5c180fb49c2be8b
benoit <benoit.koenig@accounting-for.accor.com>
2025-11-25 20:17:13 +0100

    Initialize the cdn workspace with some geo-json data

---
+++ cdn/scripts/populate-subdivisions/index.ts
@@ -0,0 +1,47 @@
+/**
+ * License: Data from geoBoundaries under CC BY 4.0
+ * Attribution: Runfola, D. et al. (2020) geoBoundaries: A global database of political administrative boundaries
+ */
+
+import { existsSync } from "node:fs";
+import { mkdir, writeFile } from "node:fs/promises";
+import path from "node:path";
+
+import { fetchFeatures } from "./fetch-features.ts";
+
+const countryCodes = ["bel", "fra", "nld", "lux"] as const;
+
+const DIR_PATH = "./public/geo-json";
+
+async function populateSubdivisions() {
+  if (!existsSync(DIR_PATH)) {
+    await mkdir(DIR_PATH, {
+      recursive: true,
+    });
+  }
+
+  for (const countryCode of countryCodes) {
+    // Wait a delay to not surcharge the API
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 500));
+
+    try {
+      const features = await fetchFeatures(countryCode);
+
+      for (const feature of features) {
+        await writeFile(
+          path.join(DIR_PATH, `${feature.properties.shapeID}.json`),
+          JSON.stringify(feature),
+        );
+      }
+
+      console.log(`✔ Subdivisions saved for ${countryCode}`);
+    } catch (err) {
+      console.error(
+        `An error occured on ${countryCode}:`,
+        err instanceof Error ? err.message : err,
+      );
+    }
+  }
+}
+
+populateSubdivisions();
